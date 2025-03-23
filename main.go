2cc1067ac50acf4ec52ca5557897f8c05e8b47db
Karn Wong <karn@karnwong.me>
2025-03-23 21:39:43 +0700

    feat: write to stream

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"os"
+	"sync"
+	"time"
+
+	"github.com/nats-io/nats.go/jetstream"
 
 	"github.com/nats-io/nats.go"
 	"github.com/rs/zerolog"
@@ -34,4 +41,70 @@ func main() {
 			log.Error().Err(err).Msg("Failed to drain NATS")
 		}
 	}(nc)
+
+	// init stream
+	js, err := jetstream.New(nc)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to JetStream")
+	} else {
+		log.Info().Msg("Connected to JetStream")
+	}
+
+	cfg := jetstream.StreamConfig{
+		Name:     "EVENTS",
+		Subjects: []string{"events.>"},
+	}
+
+	cfg.Storage = jetstream.FileStorage
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := js.CreateStream(ctx, cfg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create stream")
+	} else {
+		log.Info().Msg("Created stream")
+	}
+
+	// publish
+	loops := 1
+	iterations := 6
+
+	for range loops {
+		var wg sync.WaitGroup
+		wg.Add(iterations)
+		for range iterations {
+			go func() {
+				publish(js)
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+
+	select {
+	case <-js.PublishAsyncComplete():
+		log.Info().Msg("published 6 messages")
+	case <-time.After(time.Second):
+		log.Fatal().Msg("publish took too long")
+	}
+
+	// qa
+	printStreamState(ctx, stream)
+
+}
+
+func printStreamState(ctx context.Context, stream jetstream.Stream) {
+	info, _ := stream.Info(ctx)
+	b, _ := json.MarshalIndent(info.State, "", " ")
+	log.Info().Msg("inspecting stream info")
+	fmt.Println(string(b))
+}
+
+func publish(js jetstream.JetStream) {
+	_, err := js.PublishAsync("events.sample_input", []byte("hello world"))
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to publish async")
+	}
 }
